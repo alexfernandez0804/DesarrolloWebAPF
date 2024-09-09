22838f95ec510d129bda1c295df452dd93b2b63a
ASUS VivoBook <alexpaulfernandez0804@gmail.com>
2024-09-09 18:44:48 -0400

    Laboratorio 2 Finalizado

--- Laboratorios/Laboratorio2/ejercicio2/operaciones.php
+++ Laboratorios/Laboratorio2/ejercicio2/operaciones.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-class operaciones {
+class Operaciones {
     private $a;
     private $b;
     private $c;
@@ -27,7 +27,45 @@ if (isset($_SESSION['a']) && isset($_SESSION['b']) && isset($_SESSION['c'])) {
     $_SESSION['suma'] = $operaciones->calcularSuma();
     $_SESSION['mayor'] = $operaciones->calcularMayor();
 
-    header('Location: resultados.php');
+    $a = $_SESSION['a'];
+    $b = $_SESSION['b'];
+    $c = $_SESSION['c'];
+    $suma = $_SESSION['suma'];
+    $mayor = $_SESSION['mayor'];
+} else {
+    echo "No hay datos disponibles. Introduzca los valores primero desde el menú.";
     exit();
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultados de los Cálculos</title>
+</head>
+<body>
+    <h1>Resultados de los Cálculos</h1>
+    <table border="1" style="border-collapse: collapse; text-align: center;">
+        <tr style="background-color: red; color: white;">
+            <th>Valor de A</th>
+            <th>Valor B</th>
+            <th>Valor C</th>
+        </tr>
+        <tr>
+            <td><?php echo $a; ?></td>
+            <td><?php echo $b; ?></td>
+            <td><?php echo $c; ?></td>
+        </tr>
+        <tr>
+            <td style="background-color: red; color: white;">Suma</td>
+            <td colspan="2"><?php echo $suma; ?></td>
+        </tr>
+        <tr>
+            <td style="background-color: red; color: white;">Mayor</td>
+            <td colspan="2"><?php echo $mayor; ?></td>
+        </tr>
+    </table>
+</body>
+</html>
